7294c13233892aa6459e190dbf6b1cf52dafece5
ssujinchoi <wcsjinn@naver.com>
2023-11-21 01:09:44 +0900

    <refact> : 선택된 날짜 상태정보 변수를 하나로 변경

--- app/components/Calendar/MonthlyCalender.tsx
+++ app/components/Calendar/MonthlyCalender.tsx
@@ -21,24 +21,40 @@ export default function MonthlyCalender() {
   const dayOfWeek = ["일", "월", "화", "수", "목", "금", "토"];
 
   const [currentMonth, setCurrentMonth] = useState(new Date());
-  const [selectedYear, setSelectedYear] = useState(currentMonth.getFullYear());
-  const [selectedMonth, setSelectedMonth] = useState(currentMonth.getMonth());
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = ("0" + (today.getMonth() + 1)).slice(-2);
+  const date = ("0" + today.getDate()).slice(-2);
+
+  var dateString = year + "-" + month + "-" + date;
 
   const buildCalendarDays = () => {
-    const curMonthStartDay = new Date(selectedYear, selectedMonth, 1).getDay();
-    const curMonth = new Date(selectedYear, selectedMonth + 1, 0);
+    const selectedDate = {
+      year: currentMonth.getFullYear(),
+      month: currentMonth.getMonth(),
+    };
+    const curMonthStartDay = new Date(
+      selectedDate.year,
+      selectedDate.month,
+      1
+    ).getDay();
+    const curMonth = new Date(selectedDate.year, selectedDate.month + 1, 0);
     const curMonthEndate = curMonth.getDate();
 
-    const prevMonth = new Date(selectedYear, selectedMonth, 0);
+    const prevMonth = new Date(selectedDate.year, selectedDate.month, 0);
     const prevMonthEndDate = prevMonth.getDate();
 
     const days: Date[] = Array.from({ length: curMonthStartDay }, (_, i) => {
-      return new Date(selectedYear, selectedMonth - 1, prevMonthEndDate - i);
+      return new Date(
+        selectedDate.year,
+        selectedDate.month - 1,
+        prevMonthEndDate - i
+      );
     }).reverse();
 
     days.push(
       ...Array.from({ length: curMonthEndate }, (_, i) => {
-        return new Date(selectedYear, selectedMonth, i + 1);
+        return new Date(selectedDate.year, selectedDate.month, i + 1);
       })
     );
 
@@ -46,7 +62,7 @@ export default function MonthlyCalender() {
     if (remainDays < 7) {
       days.push(
         ...Array.from({ length: remainDays }, (_, i) => {
-          return new Date(selectedYear, selectedMonth + 1, i + 1);
+          return new Date(selectedDate.year, selectedDate.month + 1, i + 1);
         })
       );
     }
@@ -75,10 +91,6 @@ export default function MonthlyCalender() {
     );
   };
 
-  useEffect(() => {
-    setSelectedMonth(currentMonth.getMonth());
-  }, [currentMonth]);
-
   return (
     <div className="bg-white p-2.5">
       <section className="flex items-center justify-between p-4">
@@ -86,13 +98,14 @@ export default function MonthlyCalender() {
           <GrPrevious />
         </button>
         <span className="text-base">
-          {currentMonth.getFullYear()}. {currentMonth.getMonth() + 1}
+          {currentMonth.getFullYear()}.{" "}
+          {("0" + (currentMonth.getMonth() + 1)).slice(-2)}
         </span>
         <button onClick={moveNextMonth}>
           <GrNext />
         </button>
       </section>
-      <div className="grid grid-cols-7 py-2.5 px-4 text-center border-y border-gray-300">
+      <div className="grid grid-cols-7 py-2.5 px-4 font-light text-center border-y border-gray-300">
         {dayOfWeek.map((day, index) => (
           <div key={index} className={`${day === "일" ? "text-red-600" : ""}`}>
             {day}
@@ -104,7 +117,7 @@ export default function MonthlyCalender() {
               key={index}
               className={`${
                 date.getMonth() !== currentMonth.getMonth()
-                  ? "text-gray-400"
+                  ? "text-gray-300"
                   : ""
               }`}
             >
@@ -113,6 +126,7 @@ export default function MonthlyCalender() {
           );
         })}
       </div>
+      <span>{dateString}</span>
       <section className="flex flex-col mt-3">
         <div className="flex">
           {menuArr.map((tab, index) => {
